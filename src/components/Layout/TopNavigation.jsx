c4a79dd9a5be4127ad0840c71def83d1a460b9a1
Evans Ibok <evansibok@gmail.com>
2020-01-02 23:45:44 +0100

    add leftNav to top nav bar

--- src/components/Layout/TopNavigation.jsx
+++ src/components/Layout/TopNavigation.jsx
@@ -1,18 +1,57 @@
 import React from "react";
-import { Icon } from "antd";
-import styled from 'styled-components';
+import { Input, Icon } from "antd";
+import styled from "styled-components";
 
+import brandLogo from "../../images/logomark.png";
+import sheldon from "../../images/sheldon.png";
 
-const NavCon = styled.nav`
+const NavCon = styled.div`
   display: flex;
   flex-direction: row;
-  background: red;
+  background: #fff;
   width: 100vw;
   justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #c4c4c4;
 
-  nav {
+  nav.leftNav {
     display: flex;
+    margin: 1.5em;
 
+    .brandLogo {
+      padding-right: 2em;
+      border-right: 1px solid #c4c4c4;
+
+      img {
+        width: 20px;
+        height: auto;
+      }
+    }
+
+    #search {
+      margin-left: 3em;
+      padding-left: 3em;
+      border-radius: 0.3em;
+      border: none;
+      background: #f9f7ff;
+      width: 300px;
+      color: #6d6d6d;
+    }
+  }
+
+  nav.rightNav {
+    display: flex;
+    align-items: center;
+
+    p {
+      margin: 0;
+    }
+
+    img {
+      width: 30px;
+      height: auto;
+      border-radius: 50%;
+    }
   }
 `;
 
@@ -20,19 +59,23 @@ export const TopNavigation = () => {
   return (
     <NavCon>
       <nav className="leftNav">
-        <div>
-          <img src="" alt="tryble-logo" />
+        <div className="brandLogo">
+          <img src={brandLogo} alt="tryble-logo" />
         </div>
 
         <div>
-          <input type="input" placeholder="Search by project name" />
+          <Input
+            id="search"
+            placeholder="Search by project name"
+            prefix={<Icon type="search" style={{ color: "rgba(0,0,0,.25)", marginLeft: "3em"}} />}
+          />
         </div>
       </nav>
 
-      <nav>
-        <img src="" alt="" />
+      <nav className="rightNav">
+        <Icon type="inbox" />
         <p>Sheldon</p>
-        <img src="" alt="" />
+        <img src={sheldon} alt="" />
         <Icon type="caret-down" />
       </nav>
     </NavCon>
