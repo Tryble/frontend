b24ecaa858a3084bb411849c8bbb2cdaa7ca4cba
Evans Ibok <evansibok@gmail.com>
2020-01-02 22:32:13 +0100

    create base top nav layout

--- src/components/Layout/TopNavigation.jsx
+++ src/components/Layout/TopNavigation.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Icon } from "antd";
+import styled from 'styled-components';
+
+
+const NavCon = styled.nav`
+  display: flex;
+  flex-direction: row;
+  background: red;
+  width: 100vw;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+
+  }
+`;
 
 export const TopNavigation = () => {
   return (
-    <div>
-      <h1>This is the top navigation</h1>
-    </div>
-  )
-}
+    <NavCon>
+      <nav className="leftNav">
+        <div>
+          <img src="" alt="tryble-logo" />
+        </div>
+
+        <div>
+          <input type="input" placeholder="Search by project name" />
+        </div>
+      </nav>
+
+      <nav>
+        <img src="" alt="" />
+        <p>Sheldon</p>
+        <img src="" alt="" />
+        <Icon type="caret-down" />
+      </nav>
+    </NavCon>
+  );
+};
 
 export default TopNavigation;
