bf067d4a12e124f7ff61f2549e28dae32ec2133c
Evans Ibok <evansibok@gmail.com>
2020-01-02 18:38:46 +0100

    Add TopNavigation component

---
+++ src/components/Layout/TopNavigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const TopNavigation = () => {
+  return (
+    <div>
+      <h1>This is the top navigation</h1>
+    </div>
+  )
+}
+
+export default TopNavigation;
