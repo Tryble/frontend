33a9c87aaacff234e35ee6d779b8aedaf1ffa748
ofega <fegaeze@gmail.com>
2020-01-03 08:35:58 +0100

    added new route: workspace

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import Home from '../../pages/Home';
 import Login from '../../pages/Login';
+import Workspace from '../../pages/Workspace';
 import Register from '../../pages/Register';
 import AuthLayout from '../Layout/AuthLayout';
 import MainLayout from '../Layout/MainLayout';
@@ -12,6 +13,7 @@ const AppRouter = () => {
         <BrowserRouter>
             <Switch>
                 <MainLayout exact path="/" component={Home} />
+                <MainLayout path="/workspace" component={Workspace} />
                 <AuthLayout path="/login" component={Login} />
                 <AuthLayout path="/register" component={Register} />
             </Switch>
