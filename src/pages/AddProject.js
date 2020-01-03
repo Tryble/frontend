b459f89cb0aa19a7f98aa79ba3f93dc67608c271
Evans Ibok <evansibok@gmail.com>
2020-01-03 16:28:20 +0100

    add upload image section

--- src/pages/AddProject.js
+++ src/pages/AddProject.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Input } from 'antd';
+import { Input, Upload, Icon, message } from 'antd';
+
+const { Dragger } = Upload;
 
 const ProjectFormCon = styled.div`
   display: flex;
@@ -12,7 +14,8 @@ const ProjectFormCon = styled.div`
   .projectForm {
     display: flex;
     justify-content: space-between;
-    margin: 0 auto;
+    width: 800px;
+    margin: 3em;
 
     .leftForm {
       display: flex;
@@ -38,11 +41,38 @@ const ProjectFormCon = styled.div`
         }
 
       }
+
+      .rightForm {
+        display: flex;
+        flex-direction: column;
+
+        span.ant-upload.ant-upload-drag {
+          
+        }
+
+      }
     }
   }
 `;
 
 export const AddProject = () => {
+
+  const props = {
+    name: 'file',
+    multiple: true,
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
 
   return (
     <ProjectFormCon>
@@ -90,13 +120,20 @@ export const AddProject = () => {
         </div>
 
         <div className="rightForm">
-          <label>
-            upload image
-              </label>
+          <div style={{ width: "200px" }}>
 
-          <label>
+            <Dragger {...props}>
+              <p className="ant-upload-drag-icon">
+                <Icon type="inbox" />
+              </p>
+              <p className="ant-upload-text">Click or drag file to this area to upload</p>
+              <p className="ant-upload-hint">Maximum file size 12MB</p>
+            </Dragger>
+          </div>
+
+          <div>
             Date picker
-              </label>
+          </div>
         </div>
       </form>
 
