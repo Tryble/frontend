c4e7749df09e2d48dbc1b8dd9f302a062ff97f6a
Evans Ibok <evansibok@gmail.com>
2020-01-03 15:53:41 +0100

    add antd input component

--- src/pages/AddProject.js
+++ src/pages/AddProject.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Input } from 'antd';
 
 const ProjectFormCon = styled.div`
   display: flex;
-
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: #fff;
+  
   .projectForm {
     display: flex;
     justify-content: space-between;
+    margin: 0 auto;
 
     .leftForm {
+      display: flex;
       flex-direction: column;
+
+      label {
+        margin-bottom: 2em;
+
+        span {
+
+          .labelText {
+            font-weight: 600;
+          }
+        }
+
+        input {
+          padding-left: 1em;
+          border-radius: 0.3em;
+          border: none;
+          background: #f2f2f2;
+          width: 300px;
+          color: #6d6d6d;
+        }
+
+      }
     }
   }
 `;
@@ -17,32 +45,47 @@ const ProjectFormCon = styled.div`
 export const AddProject = () => {
 
   return (
-    <ProjectFormCon className="addProject">
+    <ProjectFormCon>
       <form className="projectForm">
         <div className="leftForm">
           <label>
-            Project Name
-          <input type="text" />
+            <span className="labelText">
+              Project Name
+            </span>
+            <br />
+            <Input />
           </label>
 
           <label>
-            Describe your project in one sentence
-          <input type="text" />
+            <span className="labelText">
+              Describe your project in one sentence
+            </span>
+            <br />
+            <Input />
           </label>
 
           <label>
-            Give more details about your project
-          <input type="text" />
+            <span className="labelText">
+              Give more details about your project
+            </span>
+            <br />
+            <Input />
           </label>
 
           <label>
-            Pick required roles <span>(8 roles maximum)</span>
-            <input />
+            <span className="labelText">
+              Pick required roles
+            </span>
+            <span className="supportText">(8 roles maximum)</span><br />
+            <Input />
           </label>
 
           <label>
-            Project Name
-          <input />
+            <span className="labelText">
+              Describe your project in one sentence
+            </span>
+            Project Name<br />
+            <Input />
           </label>
         </div>
 
@@ -56,6 +99,11 @@ export const AddProject = () => {
               </label>
         </div>
       </form>
+
+      <div>
+        <button>Create Project</button>
+        <button>Cancel</button>
+      </div>
 
     </ProjectFormCon>
   )
