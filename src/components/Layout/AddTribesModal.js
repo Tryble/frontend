6d3b3ebdba1f02b7824f03b73d1b118451048c95
oladimeji-ojo <ojokuredim@gmail.com>
2020-01-03 00:04:42 +0000

    latest on the sidebar and the addTribe modal

---
+++ src/components/Layout/AddTribesModal.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import { Modal, Button } from "antd";
+
+export default function AddTribesModal(props) {
+  return (
+    <div>
+      <Modal visible={props.show} onOk={props.onOk} onCancel={props.onCancel}>
+        <p> What are tribes ? </p>
+        <p>
+          Tribes are like your very own maker community. Together with your
+          tribe, you can work on projectsand build a valuable connections to
+          last a lifetime.
+        </p>
+        <p>Chose your tribe wisely!</p>
+        To createa tribe, just add a name andan emblem
+      </Modal>
+    </div>
+  );
+}
--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
-import Login from '../../pages/Login';
-import AuthLayout from '../Layout/AuthLayout';
-// import MainLayout from '../Layout/AuthLayout';
-
+import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
+import Login from "../../pages/Login";
+import AuthLayout from "../Layout/AuthLayout";
+import MainLayout from "../Layout/AuthLayout";
+import SideBar from "../Layout/Sidebar";
 
 const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <AuthLayout path="/login" component={Login} />
-            </Switch>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <AuthLayout path="/login" component={Login} />
+        <AuthLayout path="/" component={SideBar} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default AppRouter;
